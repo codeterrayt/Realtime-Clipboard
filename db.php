6e9cfd0b4037e888d0922cc1b2c6155a8bedb1c1
Code Terra <76623307+codeterrayt@users.noreply.github.com>
2024-06-27 20:22:46 +0530

    modifited for docker

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $USERNAME = "root";
 $PASSWORD = "";
 $DATABASE = "realtime_clipboard_db";
-$SERVER = "localhost";
+$SERVER = "db";
 $TABLE = "realtime_clipboard_table";
 
 $con = mysqli_connect($SERVER, $USERNAME, $PASSWORD, $DATABASE) or die("Error: " . mysqli_error($con));
