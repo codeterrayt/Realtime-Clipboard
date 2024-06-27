6b912c565b63d2be46e7e0ca1b06c681c9ae426c
Code Terra <76623307+codeterrayt@users.noreply.github.com>
2024-06-27 20:51:29 +0530

    updated for auto setup

--- index.php
+++ index.php
@@ -1,3 +1,63 @@
+<?php
+
+// phpinfo();
+
+
+
+$servername = "db";
+$username = "root";
+$password = "";
+$dbname = "realtime_clipboard_db";
+$sqlFile = './sql/realtime_clipboard_table.sql';
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Create database
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if (mysqli_query($conn, $sql)) {
+
+  // Select the database
+  mysqli_select_db($conn, $dbname);
+
+  // Read SQL file
+  $sql = file_get_contents($sqlFile);
+  if ($sql !== false) {
+    // die("Error reading SQL file\n");
+    // Execute SQL file
+    try {
+      if (mysqli_multi_query($conn, $sql)) {
+        do {
+          if ($result = mysqli_store_result($conn)) {
+            mysqli_free_result($result);
+          }
+        } while (mysqli_next_result($conn));
+        // echo "SQL script imported successfully\n";
+      } else {
+        // echo "Error importing SQL script: " . mysqli_error($conn);
+      }
+    } catch (\Throwable $th) {
+      //throw $th;
+    }
+  }
+} else {
+  echo "Error creating database: " . mysqli_error($conn);
+}
+
+
+
+// // Close connection
+// mysqli_close($conn);
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,20 +75,20 @@
 
 <body>
 
-<div class="dashboard" data-aos="flip-up" data-aos-duration="1000" id="dashboard_box">
+  <div class="dashboard" data-aos="flip-up" data-aos-duration="1000" id="dashboard_box">
     <div class="top-area">
       <h2 id="dashboard_title">Dashboard</h2>
-        <br>
-        <hr>
-        <br>
-        <button class="btn-logout btnn" id="btn-logout">Logout</button>
-        <button class="create-clipboard btnn" id="create-clipboard">Create ClipBoard</button>
-        <button class="delete-all-clipboard btnn" id="delete_all_clipboards">Delete All ClipBoards</button>
+      <br>
+      <hr>
+      <br>
+      <button class="btn-logout btnn" id="btn-logout">Logout</button>
+      <button class="create-clipboard btnn" id="create-clipboard">Create ClipBoard</button>
+      <button class="delete-all-clipboard btnn" id="delete_all_clipboards">Delete All ClipBoards</button>
 
-        <button class="show-user-id btnn" id="show-user-id">Show User ID</button>
-        <button class="open-with-clip-id btnn" id="open-with-clip-id-btn">Open with Clip ID</button>
+      <button class="show-user-id btnn" id="show-user-id">Show User ID</button>
+      <button class="open-with-clip-id btnn" id="open-with-clip-id-btn">Open with Clip ID</button>
 
-        <div style="height:70vh;  overflow-x:scroll;">
+      <div style="height:70vh;  overflow-x:scroll;">
         <table class="data-table">
           <thead>
             <tr>
@@ -39,30 +99,30 @@
             </tr>
           </thead>
           <tbody id="data-table-body">
-              <progress id="fetching_data_progress"></progress>
+            <progress id="fetching_data_progress"></progress>
           </tbody>
-          
-        </table>
-        </div>
-    
-    
-    </div>
-</div>
 
-  <div class="text-box" id="content-box" data-aos="flip-up"  data-aos-duration="1000">
+        </table>
+      </div>
+
+
+    </div>
+  </div>
+
+  <div class="text-box" id="content-box" data-aos="flip-up" data-aos-duration="1000">
     <div class="top-area">
       <h2 id="note_title">Content Title</h2>
       <h2 class="saving_text" id="saving_text">Saving...</h2>
       <div class="copy-btn" id="copy-btn" data-aos="fade-in" data-aos-delay="1000"><i class="fa-solid fa fa-copy"></i></div>
 
     </div>
-    <textarea id="main_clipboard" rows="5" wrap="virtual" spellcheck="false"> 
+    <textarea id="main_clipboard" rows="5" wrap="virtual" spellcheck="false">
       </textarea>
   </div>
-    <script src="./assets/script.js"></script>
-    <script>
-      AOS.init();
-    </script>
+  <script src="./assets/script.js"></script>
+  <script>
+    AOS.init();
+  </script>
 </body>
 
 </html>
